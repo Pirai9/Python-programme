955d2ec236cb5f2688e1ea72e741364ad8adcae4
Pirai9 <piraichselvan199@gmail.com>
2024-11-18 12:19:07 +0530

    Activity

--- Scope.py
+++ Scope.py
@@ -1,8 +1,49 @@
 name="balashanthira" #Global variable
 
 def greeting():
+    age=10   #local scope
+    print(age)
     print(name)
 
 greeting()
+# print(age) # age is not undifine 
+
+#************************1**************************************************#
+
+def greeting(firstname): 
+    age=10   #local scope
+    print(age)
+    print(name)
+    print(firstname)
+
+greeting("Ramesh")
+
+#***************************3**********************************************#
+
+def greeting(name): 
+    age=10   #local scope
+    print(age)
+    print(name)
+    print(name)
+
+greeting("Ramesh")
+
+def another():
+    print(" ")
+    greeting("Pirai") #global function greeting 
+
+another()
 
 
+#***************************4**********************************************#
+
+def another():
+    print(" ")
+    def greeting(name): #local function greeting 
+        age=10   #local scope
+        print(age)
+        print(name)
+        print(name) 
+
+another()
+#   greeting("pirai") #dont use in outside of the function
---
+++ Scope2.py
@@ -0,0 +1,15 @@
+#Scope2
+name="Piraichselvan"
+age=25
+def another():
+    global age
+    age +=2
+    color='green'
+    def greeting(name):
+        print(age)
+        color='red'
+        print(color)
+        print(name)
+    greeting("Mr_Pirai")
+
+another()
